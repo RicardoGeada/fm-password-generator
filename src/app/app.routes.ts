9171f7b4b4306b27d79e41a29aaaffd1fd7d9286
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2026-03-11 19:36:21 +0100

    feat: add generator page, set up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./pages/generator/generator').then((mod) => mod.Generator) },
+  { path: '**', loadComponent: () => import('./pages/generator/generator').then((mod) => mod.Generator) }, // unknown route redirects to generator
+];
---
+++ src/app/pages/generator/generator.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generator',
+  imports: [],
+  templateUrl: './generator.html',
+  styleUrl: './generator.scss',
+})
+export class Generator {}
