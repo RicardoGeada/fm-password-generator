f302f777ca8d036926958b291c41504c74786a02
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2026-03-18 09:29:05 +0100

    feat(password-settings): add component

--- src/app/pages/generator/generator.ts
+++ src/app/pages/generator/generator.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PasswordDisplay } from '../../components/password-display/password-display';
+import { PasswordSettings } from "../../components/password-settings/password-settings";
 
 @Component({
   selector: 'app-generator',
-  imports: [PasswordDisplay],
+  imports: [PasswordDisplay, PasswordSettings],
   templateUrl: './generator.html',
   styleUrl: './generator.scss',
 })
