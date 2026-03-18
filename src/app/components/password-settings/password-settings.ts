f489af9b64fd0cc7d252b29b2c56e1d603c94a11
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2026-03-18 17:51:29 +0100

    feat(password-settings): add character length slider

--- src/app/components/password-settings/password-settings.ts
+++ src/app/components/password-settings/password-settings.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-password-settings',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './password-settings.html',
   styleUrl: './password-settings.scss',
 })
-export class PasswordSettings {}
+export class PasswordSettings {
+
+  characterLength: number = 0;
+  maxLength: number = 20;
+
+  get sliderPercentage(): string {
+    return (this.characterLength / this.maxLength) * 100 + '%';
+  }
+
+}
