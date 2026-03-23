19bff11930b206c00972ed85de513af363765f91
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2026-03-23 09:48:54 +0100

    feat(pasword-strength): add input, render different states

--- src/app/components/password-settings/password-strength/password-strength.ts
+++ src/app/components/password-settings/password-strength/password-strength.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-password-strength',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './password-strength.html',
   styleUrl: './password-strength.scss',
 })
-export class PasswordStrength {}
+export class PasswordStrength {
+  @Input() value: '' | 'too-weak' | 'weak' | 'medium' | 'strong' = '';
+
+  get strengthText(): string {
+    return this.value == 'too-weak' ? "TOO WEAK!" : this.value;
+  }
+
+  get strengthClass(): string | null {
+    return this.value ? 'password-strength__value--' + this.value : null;
+  }
+}
