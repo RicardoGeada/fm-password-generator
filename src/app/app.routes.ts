33b32578e0bd2fa43d4f5da9e7871e150a973fb9
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2026-04-08 14:12:01 +0200

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/generator/generator').then((mod) => mod.Generator) },
-  { path: '**', loadComponent: () => import('./pages/generator/generator').then((mod) => mod.Generator) }, // unknown route redirects to generator
-];
--- src/app/types/password.type.ts
+++
@@ -1,1 +0,0 @@
-export type PasswordStrengthType = '' | 'too-weak' | 'weak' | 'medium' | 'strong';
